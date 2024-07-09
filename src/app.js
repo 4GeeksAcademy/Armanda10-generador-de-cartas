add05a409b5f9511f54666e5108cdf7d592c4d5d
ARMANDA2022 <104578464+ARMANDA2022@users.noreply.github.com>
2024-07-09 22:13:09 +0000

    trabajando en ello

--- src/app.js
+++ src/app.js
@@ -8,64 +8,115 @@ import "./assets/img/4geeks.ico";
 //Mi codigo empieza aqui
 //contruyo mi funcion igual a window.onload para que se ejecute cuando cargue mi pagina.
 //dentro de mi funcion.
-//construyo una variables nuevas con otro nombre y llamo a mis clases creada en Html.
 
 window.onload = function() {
-  const numeros = document.querySelector(".header");
-  const simbolo01 = document.querySelector(".body");
-  const simbolo02 = document.querySelector(".footer");
+  //llamo al boton del html y le aplico el evento click
 
-  //contruyo mis variables aqui les asigno el array con los sigbolo y los numeros
-  const cards = ["♦", "♥", "♠", "♣"];
-  const values = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J,",
-    "Q",
-    "K"
-  ];
+  let boton = document.getElementById("centerButton");
+  boton.addEventListener("click", function() {
+    //construyo una variables nuevas con otro nombre y llamo a mis clases creada en Html.
 
-  //declaro otra variable nueva con notro nombre y la pongo igual a mi array que voy a crear
+    const simbolo01 = document.querySelector(".header");
+    const numeros = document.querySelector(".body");
+    const simbolo02 = document.querySelector(".footer");
 
-  const simbologia = Array => {
-    //dentro de mi array declaro otra variable nueva con otro nombre y la pongo
-    //igual a la funcion math.random.
+    //contruyo mis variables aqui les asigno el array con los sigbolo y los numeros
+    const cards = ["♦", "♥", "♠", "♣"];
+    const values = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
 
-    let value = Math.floor(Math.random() * Array.length);
+    //declaro otra variable nueva con notro nombre y la pongo igual a mi array que voy a crear
 
-    return Array[value];
-  };
+    const simbologia = Array => {
+      //dentro de mi array declaro otra variable nueva con otro nombre y la pongo
+      //igual a la funcion math.random.
 
-  //fuera del array declaro otra variable nueva y la pongo igual a la variable del array "simbologia"
-  //le introduzco el parametro donde estan los dibujos de cartas.
-  //A esta variable creada le declaro la condicional "if" con una operacion logica.
+      let value = Math.floor(Math.random() * Array.length);
 
-  let raSimbolo = simbologia(cards);
-  if (raSimbolo == "♦" || raSimbolo == "♥") {
-    //Aqui dentro de la condicion llamo a mis variables.
-    //donde he creado a las clases y cambio el color de los dibujos.
+      return Array[value];
+    };
 
-    simbolo01.style.color = "red";
-    simbolo02.style.color = "red";
+    //fuera del array declaro otra variable nueva y la pongo igual a la variable del array "simbologia"
+    //le introduzco el parametro donde estan los dibujos de cartas.
+    //A esta variable creada le declaro la condicional "if" con una operacion logica.
+
+    let raSimbolo = simbologia(cards);
+    if (raSimbolo == "♦" || raSimbolo == "♥") {
+      //Aqui dentro de la condicion llamo a mis variables.
+      //donde he creado a las clases y cambio el color de los dibujos.
+
+      simbolo01.style.color = "red";
+      simbolo02.style.color = "red";
+    }
+
+    //declaro otra variable nueva y la pongo tambien igual a la variable del array "simbologia".
+    //y le introduzco el parametros donde estan los numeros y las letras.
+
+    let raNunero = simbologia(values);
+
+    //aqui llamo a mis clases creadas en Html que habia llamado antes y les asigno los valores igual a
+    //a las variables ultimas creadas que son igual a la variable del array y sus parametros.
+
+    simbolo01.innerHTML = raSimbolo;
+    numeros.innerHTML = raNunero;
+    simbolo02.innerHTML = simbolo01.innerHTML;
+  });
+
+  //creo temporizador que genere una nueva carta automatica cada 10 segundos
+
+  function generarcartas() {
+    const cabeza = document.querySelector(".header1");
+    const cuerpo = document.querySelector(".body1");
+    const pie = document.querySelector(".footer1");
+
+    const diamante = ["♦", "♥", "♠", "♣"];
+    const valores = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    const diamantes = diamante[Math.floor(Math.random() * diamantes)];
+    const valor = valores[Math.floor(Math.random() * valores.length)];
+    return `${valor} de ${diamantes}`;
   }
 
-  //declaro otra variable nueva y la pongo tambien igual a la variable del array "simbologia".
-  //y le introduzco el parametros donde estan los numeros y las letras.
+  function mostrarCartas() {
+    const carta = generarcartas();
+    document.getElementById(
+      "temporizador"
+    ).textContent = `temporizador ${carta}`;
+  }
+  //Genera una carta inmediatamente al cargar la pagina
+  mostrarCartas();
 
-  let raNunero = simbologia(values);
+  //Genera una carta cada 10 segundos (10000 milisegundos)
 
-  //aqui llamo a mis clases creadas en Html que habia llamado antes y les asigno los valores igual a
-  //a las variables ultimas creadas que son igual a la variable del array y sus parametros.
+  setInterval(mostrarCartas, 10000);
 
-  numeros.innerHTML = raSimbolo;
-  simbolo01.innerHTML = raNunero;
-  simbolo02.innerHTML = simbolo01.innerHTML;
+  // cabeza.innerHTML = diamante;
+  // cuerpo.innerHTML = valores;
+  // pie.innerHTML = cabeza.innerHTML;
 };
