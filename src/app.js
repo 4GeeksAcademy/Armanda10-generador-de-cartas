b60017a1cffc4c7efe50376e52185522e8a301fd
ARMANDA2022 <104578464+ARMANDA2022@users.noreply.github.com>
2024-07-12 22:38:01 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -6,117 +6,87 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 //Mi codigo empieza aqui
-//contruyo mi funcion igual a window.onload para que se ejecute cuando cargue mi pagina.
-//dentro de mi funcion.
+
+//Creo mi función y le asigno un nombre "generarCartas" dentro de mi función creo mis variables.
+
+function generarCartas() {
+  //construyo una variables nuevas con otro nombre y llamo a mis clases creada en Html.
+
+  const simbolo01 = document.querySelector(".header");
+  const numeros = document.querySelector(".body");
+  const simbolo02 = document.querySelector(".footer");
+
+  //contruyo mis variables les asigno el array con los sigbolo y los numeros.
+
+  const cards = ["♦", "♥", "♠", "♣"];
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  //declaro otra variable nueva con otro nombre y la pongo igual a mi array.
+
+  const simbologia = Array => {
+    //dentro de mi array declaro otra variable nueva con otro nombre y la pongo.
+    //igual a la funcion math.random.
+
+    let value = Math.floor(Math.random() * Array.length);
+    return Array[value];
+  };
+
+  //fuera del array declaro otra variable nueva y la pongo igual a la variable del array "simbologia"
+  //le introduzco el parametro donde estan los iconos de cartas (cards).
+  // le declaro la condicional "if" con una operacion logica.
+
+  let colorState = 0;
+
+  let raSimbolo = simbologia(cards);
+  if (raSimbolo == "♦" || raSimbolo == "♥") {
+    //Aqui dentro de la condicion llamo a mis variables que cree para llamar a las de Html. y les aplico style.
+    //especial mente "heard y footer" a las que quiero cambiar el color de los dos icono a rojo.
+
+    simbolo01.style.color = "red";
+    simbolo02.style.color = "red";
+  } else {
+    simbolo01.style.color = "black";
+    simbolo02.style.color = "black";
+  }
+
+  //declaro otra variable nueva y la pongo tambien igual a la variable del array "simbologia".
+  //y le introduzco el parametros donde estan los numeros y las letras (values).
+
+  let raNunero = simbologia(values);
+
+  //aqui aplico la referencia de la API Web. element.innerHTML para que me devuelva. al establecerse se reemplaza la sintaxis HTML del elemento por la nueva creada.
+  //las variables ultimas creadas que son igual a la variable del array y sus parametros.
+
+  simbolo01.innerHTML = raSimbolo;
+  numeros.innerHTML = raNunero;
+  simbolo02.innerHTML = simbolo01.innerHTML;
+}
+
+//creo mi onload fuera de la funcion  para crear mis eventos tan pronto como la pagina haya cargado.
 
 window.onload = function() {
-  //llamo al boton del html y le aplico el evento click
+  //dentro de onload creo una nueva variable y la pongo al id del boton del html y le aplico el evento click
 
   let boton = document.getElementById("centerButton");
-  boton.addEventListener("click", function() {
-    //construyo una variables nuevas con otro nombre y llamo a mis clases creada en Html.
+  boton.addEventListener("click", generarCartas);
 
-    const simbolo01 = document.querySelector(".header");
-    const numeros = document.querySelector(".body");
-    const simbolo02 = document.querySelector(".footer");
+  //Dentro del onload llamo a la función de generarCartas() y tambien la función del temporizador setInterval() que genera carta automatica segun el tiempo que pongas.
+  //Genera carta sin refrescar la pagina inmediatamente cada 5 segundos.
 
-    //contruyo mis variables aqui les asigno el array con los sigbolo y los numeros
-    const cards = ["♦", "♥", "♠", "♣"];
-    const values = [
-      "A",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K"
-    ];
-
-    //declaro otra variable nueva con notro nombre y la pongo igual a mi array que voy a crear
-
-    const simbologia = Array => {
-      //dentro de mi array declaro otra variable nueva con otro nombre y la pongo
-      //igual a la funcion math.random.
-
-      let value = Math.floor(Math.random() * Array.length);
-
-      return Array[value];
-    };
-
-    //fuera del array declaro otra variable nueva y la pongo igual a la variable del array "simbologia"
-    //le introduzco el parametro donde estan los dibujos de cartas.
-    //A esta variable creada le declaro la condicional "if" con una operacion logica.
-
-    let raSimbolo = simbologia(cards);
-    if (raSimbolo == "♦" || raSimbolo == "♥") {
-      //Aqui dentro de la condicion llamo a mis variables.
-      //donde he creado a las clases y cambio el color de los dibujos.
-
-      simbolo01.style.color = "red";
-      simbolo02.style.color = "red";
-    }
-
-    //declaro otra variable nueva y la pongo tambien igual a la variable del array "simbologia".
-    //y le introduzco el parametros donde estan los numeros y las letras.
-
-    let raNunero = simbologia(values);
-
-    //aqui llamo a mis clases creadas en Html que habia llamado antes y les asigno los valores igual a
-    //a las variables ultimas creadas que son igual a la variable del array y sus parametros.
-
-    simbolo01.innerHTML = raSimbolo;
-    numeros.innerHTML = raNunero;
-    simbolo02.innerHTML = simbolo01.innerHTML;
-  });
-
-  //creo temporizador que genere una nueva carta automatica cada 10 segundos
-
-  function generarcartas() {
-    const cabeza = document.querySelector(".header1");
-    const cuerpo = document.querySelector(".body1");
-    const pie = document.querySelector(".footer1");
-
-    const diamante = ["♦", "♥", "♠", "♣"];
-    const valores = [
-      "A",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K"
-    ];
-    const diamantes = diamante[Math.floor(Math.random() * diamantes)];
-    const valor = valores[Math.floor(Math.random() * valores.length)];
-    return `${valor} de ${diamantes}`;
-  }
-
-  function mostrarCartas() {
-    const carta = generarcartas();
-    document.getElementById(
-      "temporizador"
-    ).textContent = `temporizador ${carta}`;
-  }
-  //Genera una carta inmediatamente al cargar la pagina
-  mostrarCartas();
-
-  //Genera una carta cada 10 segundos (10000 milisegundos)
-
-  setInterval(mostrarCartas, 10000);
-
-  // cabeza.innerHTML = diamante;
-  // cuerpo.innerHTML = valores;
-  // pie.innerHTML = cabeza.innerHTML;
+  generarCartas();
+  setInterval(generarCartas, 5000);
 };
